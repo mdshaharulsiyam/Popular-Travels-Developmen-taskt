c64a52966eae88d3a676058f62c13f57e11639ea
shaharul siyam <shaharulsiyam0273@gmail.com>
2024-05-11 06:29:23 -0700

    [Pages/Ticket] loading aded

--- src/Components/ui/SelectOption/SelectOption.jsx
+++ src/Components/ui/SelectOption/SelectOption.jsx
@@ -4,9 +4,9 @@ const SelectOption = ({placeHoler,data,name}) => {
     return (
         <>
             <select name={name?name:''} className=" w-full border border-black outline-none cursor-pointer py-2">
-                <option disabled defaultValue={``} selected>{placeHoler?placeHoler:'select'}</option>
+                <option disabled value={``} selected>{placeHoler?placeHoler:'select'}</option>
                 {
-                    data ? data?.map((item,index)=><option defaultValue={item} className='cursor-pointer' key={index}>{item}</option>): <option>select option</option>
+                    data ? data?.map((item,index)=><option className='cursor-pointer' key={index}>{item}</option>): <option>select option</option>
                 }
             </select>
         </>
--- src/Pages/Ticket/Ticket.jsx
+++ src/Pages/Ticket/Ticket.jsx
@@ -9,9 +9,21 @@ const Ticket = () => {
     const [filterOptions, setFilterOptions] = useState({})
     const [ticketData, setTicketData] = useState([])
     useEffect(() => {
-        fetch(`http://localhost:5000/ticket?departure=${filterOptions?.departure}&arrival=${filterOptions?.arrival}&Pasenger=${filterOptions?.Pasenger}&date=${filterOptions?.date}`)
-            .then((res) => res.json())
-            .then((data) => setTicketData(data))
+        const fetchData = async () => {
+            const response = await toast.promise(
+                fetch(`http://localhost:5000/ticket?departure=${filterOptions?.departure}&arrival=${filterOptions?.arrival}&Pasenger=${filterOptions?.Pasenger}&date=${filterOptions?.date}`)
+                    .then((res) => res.json())
+                    .then((data) => setTicketData(data)),
+                {
+                    pending: 'please wait loading ☺',
+                    // success: 'Promise resolved 👌',
+                    error: 'somthing wrong🤯'
+                }
+            );
+            console.log(response)
+        }
+        fetchData()
+
     }, [filterOptions?.departure, filterOptions?.arrival, filterOptions?.Pasenger, filterOptions?.date])
     const HandelSubmit = (e) => {
         e.preventDefault()
@@ -22,27 +34,27 @@ const Ticket = () => {
         const filterOption = {
             departure, arrival, Pasenger, date
         }
-        // for (const key in filterOption) {
-        //     if (filterOption[key] === '') {
-        //         console.log('error')
-        //         toast.error(`please select (${key})`, {
-        //             position: "top-center",
-        //             autoClose: 5000,
-        //             hideProgressBar: false,
-        //             closeOnClick: true,
-        //             pauseOnHover: true,
-        //             draggable: true,
-        //             progress: undefined,
-        //             theme: "light",
-        //             transition: Bounce,
-        //         });
-        //         return
-        //     }
-        // }
+        for (const key in filterOption) {
+            if (filterOption[key] === '') {
+                console.log('error')
+                toast.error(`please select (${key})`, {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                    transition: Bounce,
+                });
+                return
+            }
+        }
         if (departure === arrival) {
             toast.error(`departure & arrival cannot be same`, {
                 position: "top-center",
-                autoClose: 5000,
+                autoClose: 2000,
                 hideProgressBar: false,
                 closeOnClick: true,
                 pauseOnHover: true,
@@ -53,12 +65,12 @@ const Ticket = () => {
             });
             return
         }
-        console.log(filterOption)
         setFilterOptions(filterOption)
     }
+    console.log(filterOptions)
     return (
         <>
-            <form onSubmit={HandelSubmit} className='container mx-auto py-10 sm:grid sm:grid-cols-3 justify-between lg:grid-cols-5 gap-3'>
+            <form onSubmit={HandelSubmit} className='container mx-auto py-10 sm:grid sm:grid-cols-3 justify-between lg:grid-cols-5 gap-3 flex flex-col '>
                 <Suspense fallback={`loading`}>
                     <DepartureSelect />
                 </Suspense>
@@ -68,13 +80,13 @@ const Ticket = () => {
                 <Suspense fallback={`loading`}>
                     <SeatOptions />
                 </Suspense>
-                <input type="date" name='date' className='outline-none border border-black' />
-                <button className='bg-[#312E81] rounded text-white font-bold'>search</button>
+                <input type="date" name='date' className='outline-none border border-black py-2' />
+                <button className='bg-[#312E81] rounded text-white font-bold py-2'>search</button>
             </form>
             <TicketTable ticketData={ticketData} />
             <ToastContainer
-                position="top-center"
-                autoClose={5000}
+                position="bottom-center"
+                autoClose={3000}
                 hideProgressBar={false}
                 newestOnTop={false}
                 closeOnClick
