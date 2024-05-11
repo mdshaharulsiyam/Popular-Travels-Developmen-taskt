5be8cac8422f1cf59e48c230236969c7ff45bb24
shaharul siyam <shaharulsiyam0273@gmail.com>
2024-05-11 02:00:46 -0700

    [Components/Ticket/TicketTable] fetch and show all data in table

--- src/Components/Ticket/TicketTable/TicketTable.jsx
+++ src/Components/Ticket/TicketTable/TicketTable.jsx
@@ -1,41 +1,65 @@
 import React from 'react'
 
-const TicketTable = () => {
+const TicketTable = ({ ticketData }) => {
     return (
-        <div className="overflow-x-auto">
-            <table className="table table-zebra">
+        <div className="overflow-x-auto w-full container mx-auto">
+            <table className="table table-zebra w-full">
                 {/* head */}
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Job</th>
-                        <th>Favorite Color</th>
+                <thead className='text-left'>
+                    <tr className='bg-[#E5E7EB] px-6'>
+                        <th className='uppercase text-gray-600 pl-2 py-3'>Flight</th>
+                        <th className='uppercase text-gray-600 pl-2 py-3'>AirCraft</th>
+                        <th className='uppercase text-gray-600 pl-2 py-3'>Class</th>
+                        <th className='uppercase text-gray-600 pl-2 py-3'>Fare</th>
+                        <th className='uppercase text-gray-600 pl-2 py-3'>Route</th>
+                        <th className='uppercase text-gray-600 pl-2 py-3'>Departure</th>
+                        <th className='uppercase text-gray-600 pl-2 py-3'>Arrival</th>
+                        <th className='uppercase text-gray-600 pl-2 py-3'>Duration</th>
+                        <th className='uppercase text-gray-600 pl-2 py-3'>Price</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {/* row 1 */}
-                    <tr>
-                        <th>1</th>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
-                    {/* row 2 */}
-                    <tr>
-                        <th>2</th>
-                        <td>Hart Hagerty</td>
-                        <td>Desktop Support Technician</td>
-                        <td>Purple</td>
-                    </tr>
-                    {/* row 3 */}
-                    <tr>
-                        <th>3</th>
-                        <td>Brice Swyre</td>
-                        <td>Tax Accountant</td>
-                        <td>Red</td>
-                    </tr>
+                    {ticketData?.map((item, index) => {
+                        return (
+                            <tr key={index} className={`${index % 2 !== 0 ? 'bg-[#E5E7EB]' : 'bg-[#F3F4F6]'}`}>
+                                <td className='pl-2 py-2'>
+                                    {item?.itineraries[0]?.segments[0]?.marketingCarrier} {item?.itineraries[0]?.segments[0]?.flightNumber} <br />
+                                    {item?.itineraries[0]?.segments[1]?.carrierCode} {item?.itineraries[0]?.segments[0]?.aircraft}
+                                </td>
+                                <td className='pl-2 py-2'>
+                                    {item?.itineraries[0]?.segments[0]?.aircraft} <br />
+                                    {item?.itineraries[0]?.segments[1]?.aircraft}
+                                </td>
+                                <td className='pl-2 py-2'>{item?.class[0][0]} <br />
+                                    {item?.class[0][1]}
+                                </td>
+                                <td className='pl-2 py-2'>{item?.fareBasis[0][0]} <br />
+                                    {item?.fareBasis[0][1]}
+                                </td>
+                                <td className='pl-2 py-2'>
+                                    {item?.itineraries[0]?.segments[0]?.departure?.iataCode}-{item?.itineraries[0]?.segments[0]?.arrival?.iataCode} <br />
+                                    {item?.itineraries[0]?.segments[1]?.departure?.iataCode}-{item?.itineraries[0]?.segments[0]?.arrival?.iataCode}
+                                </td >
+                                <td className='pl-2 py-2'>
+                                    {item?.itineraries[0]?.segments[0]?.departure?.at} <br />
+                                    {item?.itineraries[0]?.segments[1]?.departure?.at}
+                                </td>
+                                <td className='pl-2 py-2'>
+                                    {item?.itineraries[0]?.segments[0]?.arrival?.at} <br />
+                                    {item?.itineraries[0]?.segments[1]?.arrival?.at}
+                                </td>
+                                <td className='pl-2 py-2'>
+                                    {item?.itineraries[0]?.duration}
+                                </td>
+                                <td className='pl-2 py-2'>
+                                    {item?.price} <br />
+                                    <button className='text-xs uppercase bg-[#312e81] px-2 py-[2px] rounded text-white'>select</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
+
             </table>
         </div>
     )
--- src/Pages/Ticket/Ticket.jsx
+++ src/Pages/Ticket/Ticket.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TicketTable from '../../Components/Ticket/TicketTable/TicketTable'
 
 const Ticket = () => {
+    const [ticketData,setTicketData]=useState([])
+    useEffect(()=>{
+        fetch('http://localhost:5000/ticket')
+        .then((res)=>res.json())
+        .then((data)=>setTicketData(data))
+    },[])
   return (
     <>
-    <TicketTable/>
+    <TicketTable ticketData={ticketData}/>
     </>
   )
 }
